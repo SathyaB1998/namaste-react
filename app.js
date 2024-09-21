1a9c1d3ab3a6d4a37260c941d44864948fbadf16
B, Sathyanarayanan <Sathyanarayanan_B@comcast.com>
2024-09-21 20:09:25 +0530

    episode1

---
+++ app.js
@@ -0,0 +1,7 @@
+const parent = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+        React.createElement("h1",{},"I am a h1 tag")));
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
